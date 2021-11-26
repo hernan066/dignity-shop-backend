2ae74ff16b5fb629727524dc331a23e9a0f8ee8d
hernangit config --global user.name John <hernan0066@gmail.com>
2021-11-26 14:22:02 -0300

    Cambios auth res

--- routes/auth.js
+++ routes/auth.js
@@ -24,45 +24,52 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 
-router.post('/login', async (req, res) => {
-    try{
-        const user = await User.findOne(
-            {
-                userName: req.body.user_name
-            }
-        );
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      userName: req.body.user_name,
+    });
 
-        !user && res.status(401).json("Wrong User Name");
+    !user &&
+      res.status(401).json({
+        ok: false,
+        msg: "El usuario no existe con ese email",
+      });
 
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
 
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const inputPassword = req.body.password;
 
-        const inputPassword = req.body.password;
-        
-        originalPassword != inputPassword && 
-            res.status(401).json("Wrong Password");
+    originalPassword != inputPassword &&
+      res.status(401).json({
+        ok: false,
+        msg: "Contraseña incorrecta",
+      });
+    
+      //Generar token
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
 
-        const accessToken = jwt.sign(
-        {
-            id: user._id,
-            isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-            {expiresIn:"3d"}
-        );
-  
-        const { password, ...others } = user._doc;  
-        res.status(200).json({...others, accessToken});
-
-    }catch(err){
-        res.status(500).json(err);
-    }
-
+    const { password, ...others } = user._doc;
+    res.status(200).json({
+      ...others,
+      accessToken,
+      ok: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
